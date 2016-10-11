a439cb5b68aa0920a6cca132f7e7f1469f5eb496
Usuario <Usuario@172.16.58.180>
2016-10-11 09:33:52 -0300

    Interface

--- src/SymbolTable.java
+++ src/SymbolTable.java
@@ -11,5 +11,21 @@
  */
 public interface SymbolTable<K extends Comparable<K>,V>
 {
-    
+    public void put(K key, V val);
+    public V get(K key);
+    public void delete(K key);
+    public boolean contains(K key);
+    public boolean isEmpty();
+    public int size();
+    public K min();
+    public K max();
+    public K floor(K key);
+    public K ceiling(K key);
+    public int rank(K key);
+    public K select(int k);
+    public void deleteMin();
+    public void deleteMax();
+    public int size(K lo, K hi);
+    Iterable<K> keys(K lo, K hi);
+    Iterable<K> keys();
 }
